0edfccbba9daa2e2ea76caf3cb392e8cd4905179
aliaksandr-mishuhou <>
2021-03-27 12:44:32 +0300

    updated stats API

--- realt-stats-api/src/main/java/com/realt/statsapi/data/StatsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.realt.statsapi.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDate;
-
-public interface StatsRepository extends JpaRepository<DailyItem, LocalDate> {
-}
